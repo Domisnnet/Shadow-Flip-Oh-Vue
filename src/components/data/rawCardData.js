af08bcbf8f6b282599cea09b9b4401d5b4b4c7a8
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-10-31 12:02:24 +0000

    populando os dados

---
+++ src/components/data/rawCardData.js
@@ -0,0 +1,82 @@
+
+export const RAW_CARD_DATA = [
+    {
+        pairValue: 1, 
+        nome: "Cosmos Sentinel, o Guardião Galáctico",
+        fundo: "fundo-1",
+        nivel: 5, 
+        imagem: "cosmos-sentinel.png",
+        alt: "Cosmos Sentinel, o Guardião Galáctico",
+        descricao: "Este imponente guerreiro cibernético é a personificação da harmonia...",
+        atk: 2800,
+        def: 2500
+    },
+    {
+        pairValue: 2,
+        nome: "Nebuladragon, o Ser das Estrelas Eternas",
+        fundo: "fundo-2",
+        nivel: 7,
+        imagem: "nebuladragon.png",
+        alt: "Nebuladragon, o Ser das Estrelas Eternas",
+        descricao: "Uma criatura mítica nascida da fusão de nebulosas e energia estelar...",
+        atk: 3200,
+        def: 2000
+    },
+    {
+        pairValue: 3,
+        nome: "Cyberblade Paladin, o Cavaleiro Digital",
+        fundo: "fundo-3",
+        nivel: 3,
+        imagem: "cyberblade-paladin.png",
+        alt: "Cyberblade Paladin, o Cavaleiro Digital",
+        descricao: "Armado com a lendária Cyberblade, este cavaleiro cibernético é um protetor...",
+        atk: 2600,
+        def: 2300
+    },
+    {
+        pairValue: 4,
+        nome: "Mechadragon X, o Destruidor Biomecânico",
+        fundo: "fundo-4",
+        nivel: 7,
+        imagem: "mechadragon.png",
+        alt: "Mechadragon X, o Destruidor Biomecânico",
+        descricao: "Uma fusão mortal de biologia dracônica e engenharia cibernética...",
+        atk: 3500,
+        def: 3000
+    },
+    {
+        pairValue: 5,
+        nome: "Archmage Stellarion, o Guardião das Estrelas",
+        fundo: "fundo-5",
+        nivel: 3,
+        imagem: "archmage.png",
+        alt: "Archmage Stellarion, o Guardião das Estrelas",
+        descricao: "Um mago enigmático que manipula as forças do cosmos para proteger o equilíbrio...",
+        atk: 2400,
+        def: 2600
+    },
+    {
+        pairValue: 6,
+        nome: "Aegis Knight, o Guardião do Firmamento",
+        fundo: "fundo-6",
+        nivel: 4,
+        imagem: "aegis-knight.png",
+        alt: "Aegis Knight, o Guardião do Firmamento",
+        descricao: "Vestindo uma armadura forjada com fragmentos de estrelas, Aegis Knight...",
+        atk: 3000,
+        def: 2800
+    },
+    {
+        pairValue: 7,
+        nome: "Stormbringer Dragon, o Arauto das Tempestades",
+        fundo: "fundo-7",
+        nivel: 5,
+        imagem: "stormbringer.png",
+        alt: "Stormbringer Dragon, o Arauto das Tempestades",
+        descricao: "Este poderoso dragão domina os céus, invocando trovões e relâmpagos...",
+        atk: 2900,
+        def: 2100
+    }
+];
+
+export default RAW_CARD_DATA;
